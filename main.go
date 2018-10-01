2140da4df0e9692ecee527b3ba715db9a6bd8b57
Canino Developments <ian.canino@gmail.com>
2018-10-01 11:46:38 -0400

    immplemented /api/articles endopint

--- main.go
+++ main.go
@@ -2,48 +2,57 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
-	"fmt"
-	"os"
-	_ "github.com/lib/pq"
+	"time"
 )
 
 var db *sql.DB
 
 const (
 	dbhost = "localhost"
-	dbport = 5432
+	dbport = "5432"
 	dbuser = "caninodev"
-	dbpass = "80LgjYjvq6zcztiz"
-	dbname = "lecheleccion-api_development"
+	dbpass = "QpPkW4jkgLc1"
+	dbname = "lecheleccion"
 )
+
+type Article struct {
+	ID                  int
+	URL                 string
+	UrlToImage          string
+	Source              string
+	PublicationDate     time.Time
+	Title               string
+	Body                string
+	ExternalReferenceID string
+	createdAt           time.Time
+	UpdatedAt           time.Time
+}
+
+type Collection struct {
+	Index []Article
+}
 
 func main() {
 	initDb()
-	defer db.Close()
 	http.HandleFunc("/api/articles", articlesHandler)
-	http.HandleFunc("/api/users", usersHandler)
+	//http.HandleFunc("/api/users", usersHandler)
+	defer db.Close()
+
 	log.Fatal(http.ListenAndServe("localhost:3001", nil))
 }
 
-func articlesHandler(w http.ResponseWriter, r *http.Request) {
-	// ...
-}
-
-func usersHandler(w http.ResponseWriter, r *http.Request) {
-	// ...
-}
-
-func initDB() {
-	config := dbConfig()
+func initDb() {
 	var err error
-	psqlInfov := fmt.Sprint("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-	dbhost, dbport,
-	dbuser, dbpass, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbhost, dbport,
+		dbuser, dbpass, dbname)
 
-	db, err = sql.Open("postgres", psqlInfov)
+	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
@@ -52,6 +61,64 @@ func initDB() {
 		panic(err)
 	}
 	fmt.Println("Successfully connected!")
+}
+
+func articlesHandler(w http.ResponseWriter, r *http.Request) {
+	//var (
+	//	ID, external_reference_id int
+	//	URL, UrlToImage, Source, Body, Title, created_at, updated_ad string
+	//
+	//
+	collection := Collection{}
+
+	err := queryArticles(&collection)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	out, err := json.Marshal(collection)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	fmt.Fprintf(w, string(out))
+}
+
+func queryArticles(collection *Collection) error {
+	rows, err := db.Query(`SELECT t.* FROM collections.articles t LIMIT 50`)
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		article := Article{}
+		err = rows.Scan(
+			&article.ID,
+			&article.URL,
+			&article.UrlToImage,
+			&article.Source,
+			&article.PublicationDate,
+			&article.Title,
+			&article.Body,
+			&article.ExternalReferenceID,
+			&article.createdAt,
+			&article.UpdatedAt)
+		if err != nil {
+			return err
+		}
+
+		collection.Index = append(collection.Index, article)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 
